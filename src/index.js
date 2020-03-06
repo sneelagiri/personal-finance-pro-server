dd75fce4bf22cadc6ede3d7860024777058f4530
Shashank Neelagiri <sneelagiri@outlook.com>
2020-03-06 16:26:55 +0100

    added resolvers which will be used in the GraphQL server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
-
-const resolvers = {};
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Budget = require("./resolvers/Budget");
+const Expense = require("./resolvers/Expense");
+const Savings = require("./resolvers/Savings");
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Budget,
+  Expense,
+  Savings
+};
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
